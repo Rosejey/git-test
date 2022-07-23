a73a4dffa398a7d577cb29608f2d76a9ee0cc795
Rose <Rose@163.com>
2022-07-23 10:38:35 +0800

    third commit

--- src/main/java/com/li/GitTest.java
+++ src/main/java/com/li/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git!!!");
         System.out.println("Git!!!2");
+        System.out.println("Git!!!3");
     }
 
 }
